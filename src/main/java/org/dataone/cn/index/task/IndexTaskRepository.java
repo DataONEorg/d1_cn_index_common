113bf35f5c082cefdde57c20f8d1ea0e17a99f15
Sky Roseboom <sroseboo@epscor.unm.edu>
2011-12-15 18:39:42 +0000

    turning off auto-ddl generation for index task repository postgres data source.

--- src/main/java/org/dataone/cn/index/task/IndexTaskRepository.java
+++ src/main/java/org/dataone/cn/index/task/IndexTaskRepository.java
@@ -2,8 +2,6 @@ package org.dataone.cn.index.task;
 
 import java.util.List;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -13,9 +11,6 @@ public interface IndexTaskRepository extends JpaRepository<IndexTask, Long> {
     List<IndexTask> findByPid(String pid);
 
     List<IndexTask> findByPidAndStatus(String pid, String status);
-
-    Page<IndexTask> findByStatusOrderByPriorityAscTaskModifiedDateAsc(String status,
-            Pageable pageable);
 
     List<IndexTask> findByStatusOrderByPriorityAscTaskModifiedDateAsc(String status);
 
