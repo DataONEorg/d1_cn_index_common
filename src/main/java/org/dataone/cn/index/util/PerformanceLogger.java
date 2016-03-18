238cbed4883141dfe67e5d95da421eb0802a3081
Andrei Buium <andreib@dataone.unm.edu>
2016-03-18 18:57:12 +0000

    Reintroduced a PerformanceLogger (no longer a bean for injection though - that was causing problems with metacat last time) and made the performance logging disable-able via a parameter in index-processor.properties

---
+++ src/main/java/org/dataone/cn/index/util/PerformanceLogger.java
@@ -0,0 +1,43 @@
+package org.dataone.cn.index.util;
+
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.dataone.configuration.Settings;
+
+public class PerformanceLogger {
+
+    private static PerformanceLogger self = new PerformanceLogger(); // non-lazy singleton 
+    private static final Level LOGGING_LEVEL = Level.INFO;
+    private static Logger perfLogger;
+    private static boolean enabled;
+    
+    private PerformanceLogger() {
+        Boolean enabledProperty = Settings.getConfiguration().getBoolean("dataone.indexing.performance.logging.enabled");
+        if (enabledProperty == null || enabledProperty.equals(false))
+            enabled = false;
+        else 
+            enabled = true;
+        
+        perfLogger = Logger.getLogger("performanceStats");
+        
+        if (perfLogger == null) {
+            Logger defaultLogger = Logger.getLogger(PerformanceLogger.class.getName());
+            defaultLogger.error("Unable to create Logger for performanceStats appender!");
+            enabled = false;
+        }
+    }
+    
+    public static PerformanceLogger getInstance() {
+        return self;
+    }
+    
+    public void log(String id, long milliseconds) {
+        if (enabled)
+            log("" + id + ", " + milliseconds);
+    }
+    
+    public void log(String message) {
+        if (enabled)
+            perfLogger.log(LOGGING_LEVEL, message);
+    }
+}
