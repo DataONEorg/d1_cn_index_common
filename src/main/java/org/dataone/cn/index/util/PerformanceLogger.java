e45097b52dfffc188dca4a9f2f696bb802b21dbe
Rob Nahf <rnahf@epscor.unm.edu>
2018-04-26 18:34:03 +0000

    refactored default logging for performanceLogger.  possible bug fix on the name of the class of the default logger.

--- src/main/java/org/dataone/cn/index/util/PerformanceLogger.java
+++ src/main/java/org/dataone/cn/index/util/PerformanceLogger.java
@@ -6,17 +6,21 @@ import org.dataone.configuration.Settings;
 
 public class PerformanceLogger {
 
+    static final Logger defaultLogger = Logger.getLogger(PerformanceLogger.class);
+	
     private static PerformanceLogger self = new PerformanceLogger(); // non-lazy singleton 
     private static final Level LOGGING_LEVEL = Level.INFO;
     private static Logger perfLogger;
     private static boolean enabled;
+
     
     private PerformanceLogger() {
         enabled = Settings.getConfiguration().getBoolean("dataone.indexing.performance.logging.enabled", Boolean.FALSE);
+        defaultLogger.warn("Setting up PerformanceLogger: set to enabled? " + enabled);
+        
         perfLogger = Logger.getLogger("performanceStats");
         
         if (perfLogger == null) {
-            Logger defaultLogger = Logger.getLogger(PerformanceLogger.class.getName());
             defaultLogger.error("Unable to create Logger for performanceStats appender!");
             enabled = false;
         }
